713e2299f6a944f47959e31512b7452eeb2ca147
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2024-04-15 22:03:26 +0200

    finished

--- simple_nn.py
+++ simple_nn.py
@@ -2,3 +2,57 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+dataset = np.loadtxt('pima-indians-diabetes.csv', delimiter=',')
+X = dataset[:, 0:8]
+y = dataset[:, 8]
+
+X = torch.tensor(X, dtype=torch.float32)
+y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
+
+class PimaClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden1 = nn.Linear(8, 12)
+        self.act1 = nn.ReLU()
+        self.hidden2 = nn.Linear(12, 8)
+        self.act2 = nn.ReLU()
+        self.output = nn.Linear(8, 1)
+        self.act_output = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.act1(self.hidden1(x))
+        x = self.act2(self.hidden2(x))
+        x = self.act_output(self.output(x))
+        return x
+    
+model = PimaClassifier()
+print(model)
+
+loss_fn = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+n_epochs = 100
+batch_size = 10
+
+for epoch in range(n_epochs):
+    for i in range(0, len(X), batch_size):
+        X_batch = X[i:i+batch_size]
+        y_pred = model(X_batch)
+        y_batch = y[i:i+batch_size]
+        loss = loss_fn(y_pred, y_batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f'Finished epoch {epoch}, latest loss {loss}')
+
+with torch.no_grad():
+    y_pred = model(X)
+
+accuracy = (y_pred.round() == y).float().mean()
+print(f'Accuracy: {accuracy}')
+
+predictions = (model(X) > 0.5).int()
+for i in range(5):
+    print(f"{X[i].tolist()} --> {predictions[i]} (expected: {y[i]})")
+    
